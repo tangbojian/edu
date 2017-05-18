11e1a52900e5abc0599d04c6cf1d5f963291569c
tangbojian <444325459@qq.com>
2017-05-18 10:53:53 +0800

    添加 userMapper 接口

---
+++ edu.dao/src/main/java/com/tbj/edu/dao/user/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.tbj.edu.dao.user.mapper;
+
+/**
+ * 
+ * @author bjtang
+ * @date   2017Äê5ÔÂ18ÈÕ  
+ * @desc   userMapper
+ */
+public interface UserMapper {
+
+	int insert(Object model);
+	
+		
+}
