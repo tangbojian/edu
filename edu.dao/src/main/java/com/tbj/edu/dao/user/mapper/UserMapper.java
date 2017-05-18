b0c0a5c17ad63698d332a4ddb355ac39788b763e
tangbojian <444325459@qq.com>
2017-05-18 17:50:39 +0800

    commo

--- edu.dao/src/main/java/com/tbj/edu/dao/user/mapper/UserMapper.java
+++ edu.dao/src/main/java/com/tbj/edu/dao/user/mapper/UserMapper.java
@@ -1,14 +1,16 @@
 package com.tbj.edu.dao.user.mapper;
 
+import com.tbj.common.dao.CommonMapper;
+import com.tbj.edu.entity.User;
+
 /**
  * 
  * @author bjtang
  * @date   2017Äê5ÔÂ18ÈÕ  
  * @desc   userMapper
  */
-public interface UserMapper {
+public interface UserMapper extends CommonMapper<User>{
 
-	int insert(Object model);
 	
 		
 }
--- edu.web/src/main/java/com/tbj/edu/web/controller/user/UserController.java
+++ edu.web/src/main/java/com/tbj/edu/web/controller/user/UserController.java
@@ -1,13 +1,22 @@
 package com.tbj.edu.web.controller.user;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * 
  * @author bjtang
  * @date   2017Äê5ÔÂ18ÈÕ  
  * @desc
  */
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
 
-	
+	@RequestMapping("/index")
+	public void index(){
+		System.out.println("index");
+	}
 	
 }
