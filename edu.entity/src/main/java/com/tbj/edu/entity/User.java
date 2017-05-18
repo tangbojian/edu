3283ce989105465a22309514d5e883de0e22eb2f
tangbojian <444325459@qq.com>
2017-05-18 16:37:08 +0800

    add edu.service add.entity

---
+++ edu.entity/src/main/java/com/tbj/edu/entity/User.java
@@ -0,0 +1,39 @@
+package com.tbj.edu.entity;
+
+/**
+ * 
+ * @author bjtang
+ * @date 2017Äê5ÔÂ18ÈÕ
+ * @desc
+ */
+public class User {
+
+	private int id;
+	private int age;
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ edu.service/src/main/java/com/tbj/edu/service/user/UserService.java
@@ -0,0 +1,10 @@
+package com.tbj.edu.service.user;
+/**
+ * 
+ * @author bjtang
+ * @date   2017Äê5ÔÂ18ÈÕ  
+ * @desc
+ */
+public interface UserService {
+
+}
